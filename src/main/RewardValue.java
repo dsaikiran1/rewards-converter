588c52903b8b3dad71e47deebce67a8ad73490ff
D Saikiran <138295409+dsaikiran1@users.noreply.github.com>
2024-08-03 17:06:55 +0530

    Create RewardValue.java
    17:06

---
+++ src/main/RewardValue.java
@@ -0,0 +1,19 @@
+public class RewardValue{
+  private int cash;
+  private int miles;
+  private static final miles_to_cash_rate = 0.0035; 
+  public RewardValue(int cash){
+    this.cash = cash;
+    this.miles = cash/miles_to_cash_rate; 
+  }
+public RewardValue(int miles){
+this.cash = miles*miles_to_cash_rate;
+  this.miles = miles;
+}
+public static int getCashValue(){
+return cash;
+}
+public static int getMilesValue(){
+return miles;
+}
+}
