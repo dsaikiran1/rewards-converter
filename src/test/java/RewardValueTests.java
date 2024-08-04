8dbb0a57ef2dc9b0cf07b6e9957f0e28d81f23c0
D Saikiran <138295409+dsaikiran1@users.noreply.github.com>
2024-08-04 22:44:15 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
 
+       private static final double miles_to_cash_rate = 0.0035;
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
@@ -14,17 +15,25 @@ public class RewardValueTests {
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        var miles = new MilesValue(milesValue);
+        assertEquals(milesValue, miles.getMilesValue());
     }
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        var rewardValue = new RewardValue(cashValue);
+        double milesValue = cashValue/miles_to_cash_rate;
+        assertEquals(milesValue,rewardValue.getMilesValue());    
+        //assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 100;
+        var miles = new MilesValue(milesValue);
+        double cashValue = milesValue*miles_to_cash_rate;
+        assertEquals(cashValue,miles.getCashValue());
+        //assert false;
     }
 }
